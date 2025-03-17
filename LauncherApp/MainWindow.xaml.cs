0f52338d7ef7e98a91cdcad4f31097267c4da123
MimeiChan <147392562+MimeiChan@users.noreply.github.com>
2025-03-18 01:00:35 +0900

    Simplify window handle access using P/Invoke

--- LauncherApp/MainWindow.xaml.cs
+++ LauncherApp/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Media;
 using System;
 using System.Runtime.InteropServices;
-using WinRT;
 using WinRT.Interop;
 
 namespace LauncherApp
@@ -17,69 +16,48 @@ namespace LauncherApp
         {
             InitializeComponent();
 
-            // Set window size
-            SetWindowSize(1200, 800);
-
             // Set custom title bar
             ExtendsContentIntoTitleBar = true;
             SetTitleBar(AppTitleBar);
 
+            // Get window handle using the WinRT Interop helper
+            IntPtr hWnd = GetWindowHandle();
+            
             // Get AppWindow object
-            IntPtr hWnd = WindowNative.GetWindowHandle(this);
-            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
             _appWindow = AppWindow.GetFromWindowId(windowId);
 
             // Set window title
             Title = "業務ランチャー";
+            
+            // Set window size
+            SetWindowSize(1200, 800);
+        }
+
+        private IntPtr GetWindowHandle()
+        {
+            var windowHandle = WindowNative.GetWindowHandle(this);
+            return windowHandle;
         }
 
         private void SetWindowSize(int width, int height)
         {
-            // Get handle to the core window
-            IntPtr hWnd = WindowNative.GetWindowHandle(this);
-            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
-            _appWindow = AppWindow.GetFromWindowId(windowId);
-
-            var size = new Windows.Graphics.SizeInt32();
-            size.Width = width;
-            size.Height = height;
-            _appWindow.Resize(size);
+            if (_appWindow != null)
+            {
+                var size = new Windows.Graphics.SizeInt32
+                {
+                    Width = width,
+                    Height = height
+                };
+                _appWindow.Resize(size);
+            }
         }
     }
 
-    // ネイティブウィンドウハンドル取得用のユーティリティクラス
-    public static class WindowNative
+    // P/Invoke for getting the window handle
+    internal class WindowNative
     {
-        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto, PreserveSig = true, SetLastError = false)]
-        public static extern IntPtr GetActiveWindow();
-
-        public static IntPtr GetWindowHandle(object window)
-        {
-            var windowsBaseWindow = window as Microsoft.UI.Xaml.Window;
-            if (windowsBaseWindow == null)
-                return IntPtr.Zero;
-
-            // Handle is an IntPtr value (HWND) cast to a int value
-            dynamic interopWindowHandle = TypeHelper.As<IWindowNative>(windowsBaseWindow);
-            return interopWindowHandle.WindowHandle;
-        }
-    }
-
-    // 型ヘルパー (WinRT統合用)
-    internal static class TypeHelper
-    {
-        public static T As<T>(object obj)
-        {
-            return (T)obj.As<T>();
-        }
-    }
-
-    // COM インターフェース定義
-    [ComImport]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
-    internal interface IWindowNative
-    {
-        IntPtr WindowHandle { get; }
+        [DllImport("Microsoft.ui.xaml.dll")]
+        public static extern IntPtr GetWindowHandle(Window window);
     }
 }
